6eca8b688c7499b1c9c6da944911fa3f114ec37a
Pawan-chouhan <pawanchouhan1575@gmail.com>
2026-07-21 13:19:48 +0530

    Time: 2 ms (92.12%), Space: 47.9 MB (38.57%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,16 +1,15 @@
 class Solution {
     public void moveZeroes(int[] arr) {
-        int i=0;
-        for(int j=0;j<arr.length;j++){
-            if(arr[j]!=0){
-                arr[i]=arr[j];
-                i++;
+        int j=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]!=0){
+                arr[j]=arr[i];
+                j++;
             }
         }
-        while(i<arr.length){
-            arr[i]=0;
-            i++;
+        while(j<arr.length){
+            arr[j]=0;
+            j++;
         }
-        
     }
 }
