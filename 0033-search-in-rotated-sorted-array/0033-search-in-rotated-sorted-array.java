e76e609e94dca51eb0553001cafcba4bacce1966
Pawan-chouhan <pawanchouhan1575@gmail.com>
2026-09-23 14:13:18 +0530

    Time: 0 ms (100%), Space: 43.9 MB (47.25%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -5,17 +5,16 @@ class Solution {
         int e = n-1;
         while(s<=e){
             int mid = s+(e-s)/2;
-            if(arr[mid ]==target)return mid;
-           else  if(arr[mid]>=arr[s]){
-                if(target>=arr[s]&&target<arr[mid])e= mid-1;
-                else s =mid+1;
+            if(arr[mid]==target) return mid;
+            else if(arr[mid]>=arr[s]){
+                if(arr[mid]>target&&arr[s]<=target)e= mid-1;
+                else s= mid+1;
             }
-            else {
-                if(arr[mid]<target&&arr[e]>=target){
-                    s = mid+1;
-                }
-                else e = mid-1;
-            }
+        else{
+            if(arr[mid]<=target&&arr[e]>=target) s= mid+1;
+            else e = mid-1;
+        }
+        
         }
         return -1;
     }
